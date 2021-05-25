61cd61ac92fcc9120003363ccea71ffb65ee3739
lodenset <lodenset@gmail.com>
2021-05-25 09:40:32 +0300

    #3 build: Change css to scss

--- FrontEndDevelopmentLibraries/markdown-previewer/src/App.js
+++ FrontEndDevelopmentLibraries/markdown-previewer/src/App.js
@@ -1,10 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div></div>
+
+  const [editedText, setEditedText] = useState('');
+
+  const [previewText, setPreviewText] = useState('');
+
+  function handleChange(e){
+    setEditedText(e.target.value);
+  }
+
+  const Editor = () => {
+    return(
+      <textarea
+        value={editedText} 
+        id='editor'
+        type='text'
+        onChange = {handleChange}
+      />
     );
+  }
+
+  const Previewer = () => {
+    return(
+      <div id="preview">
+
+      </div>
+    )
+  }
+
+  return (
+    <div>      
+      <Editor/>
+      <Previewer/>
+    </div>
+  );
 }
 
 export default App;
